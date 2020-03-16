ebe0b0ff349ed14c05c983b88df54b5fadf762c5
Zoltan Illes <zoltan3illes@gmail.com>
2020-03-16 19:18:29 +0100

    BLOGHW-002

--- controllers/login.js
+++ controllers/login.js
@@ -4,17 +4,30 @@ const userData = {
     userDataPassword: 'admin'
 }
 
-const warning = 'Error: invalid credentials !'
+
 
 module.exports = class Login {
     getLogin(req, res) {
         const {username, password} = req.body;
+        let warning = ''
         
 
         if(username === userData.userDataName && password === userData.userDataPassword){
             res.render('admin')
         }else{
-            res.render('login', {warning});
+            if(!username){
+                warning = 'Username required !'
+            }
+            else if(username !== userData.userDataName){
+                warning = 'Wrong user name !'
+            }
+            else if(!password){
+                warning = 'Password required !'
+            }
+            else if(password !== userData.userDataPassword){
+                warning = 'Wrong password !'
+            }
+                res.render('login', {warning});
         }
     }
 }
